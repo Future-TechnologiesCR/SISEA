9027d33eca1fbc5224bb6786373bfaaf96034927
McFly <kencamacho19@gmail.com>
2022-09-22 20:42:52 -0600

    pedro ricoo

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -6,9 +6,9 @@ function Contacto() {
         <>
             <div id='Contacto'>
                 <h1 className='titulo-contacto'>Contactenos</h1>
-                <div className='Wrapper'>
-                    <div className='inner'>
-                        <div className='col1'>
+                <div className='contacto'>
+                    <div className='info-contacto'>
+                        <div className='cuadro1'>
                             <h5><b>Solicite una cita</b></h5>
                             <p>Oficinas: 8:00 AM - 5:00 PM</p>
                             <p>Monitoreo 24 horas</p>
@@ -20,88 +20,100 @@ function Contacto() {
                             <h5><b>Encuéntrenos</b></h5>
                             <p>Dirección física: De Multiplaza, 800 metros este y 200 metros norte edificio K1, Curridabat, San José, Costa Rica</p>
                         </div>
-                        <div className='formulario'>
-                            <section className="contact section scroll-spy-section" id="contact">
-                                <h2 className="heading-underline">PREGUNTANOS DIRECTAMENTE</h2>
-                                <p>CONSULTAS, PRESUPUESTOS, COMPRAS, O AFLIACIONES </p>
-                                <div className="wrapper">
-                                    <form
-                                        action="https://formsubmit.co/pedroborbonserrano@gmail.com"
-                                        method="POST"
-                                        className="contact__form"
+                    <div className='formulario'>
+                        <section className="contact section scroll-spy-section" id="contact">
+                            <h2 className="heading-underline">PREGUNTANOS DIRECTAMENTE</h2>
+                            <p>CONSULTAS, PRESUPUESTOS, COMPRAS, O AFLIACIONES </p>
+                            <div className="wrapper">
+                                <form
+                                    action="https://formsubmit.co/pedroborbonserrano@gmail.com"
+                                    method="POST"
+                                    className="contact__form"
                                     >
-                                        <input
-                                            type="hidden"
-                                            name="_subject"
-                                            value="Correo proveniente de la página!"
+                                    <input
+                                        type="hidden"
+                                        name="_subject"
+                                        value="Correo proveniente de la página!"
+                                    />
+                                    <input
+                                        type="hidden"
+                                        name="_next"
+                                        value="#"
                                         />
-                                        <input
-                                            type="hidden"
-                                            name="_next"
-                                            value="#"
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        id="name"
+                                        required
+                                        className="contact__form-input"
+                                        placeholder='Nombre:'
                                         />
-                                        <input
-                                            type="text"
-                                            name="name"
-                                            id="name"
-                                            required
-                                            className="contact__form-input"
-                                            placeholder='Nombre:'
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        required
+                                        className="contact__form-input"
+                                        placeholder='Emalil:'
                                         />
-                                        <input
-                                            type="email"
-                                            name="email"
-                                            id="email"
-                                            required
-                                            className="contact__form-input"
-                                            placeholder='Emalil:'
-                                        />
-                                        <textarea
-                                            type="text"
-                                            name="message"
-                                            id="msg"
-                                            required
-                                            className="contact__form-input"
-                                            placeholder='Mensaje:'
-                                        />
-                                        <button type="submit" className="btn contact__form-btn">
-                                            ENVIAR
-                                            <div className="shadow"></div>
-                                        </button>
-                                        <input type="hidden" name="_next" value="http://127.0.0.1:5173/"></input>
-                                    </form>
-                                </div>
-                                <div className="container">
-                                    <div className='wraper'>
-                                        <hr />
-                                        <h5><b>Comuniquese</b></h5>
-                                        <div className="button"
-                                            onClick={() => {
-                                                window.open("https://www.instagram.com/sisea_seguridad", "_blank");
-                                            }}>
-                                            <div className="icon"><i className="fab fa-instagram"></i></div>
-                                            <span>Instagram</span>
-                                        </div>
-                                        <div className="button"
-                                            onClick={() => {
-                                                window.open("https://www.facebook.com/SISEA", "_blank");
-                                            }}
-                                        >
-                                            <div className="icon"><i className="fab fa-facebook"></i></div>
-                                            <span>facebook</span>
-                                        </div>
-                                        <div className="button"
-                                            onClick={() => {
-                                                window.open("https://api.whatsapp.com/send?phone=50672920011", "_blank");
-                                            }}
-                                        >
-                                            <div className="icon"><i className="fab fa-whatsapp"></i></div>
-                                            <span>whatsapp</span>
-                                        </div>
+                                    <textarea
+                                        type="text"
+                                        name="message"
+                                        id="msg"
+                                        required
+                                        className="contact__form-input"
+                                        placeholder='Mensaje:'
+                                    />
+                                    <button type="submit" className="btn contact__form-btn">
+                                        ENVIAR
+                                        <div className="shadow"></div>
+                                    </button>
+                                    <input type="hidden" name="_next" value="http://127.0.0.1:5173/"></input>
+                                </form>
+                            </div>
+                            <div className="block-left"></div>
+                            <div className="container">
+                                <div className='wraper'>
+                                    <hr />
+                                    <h5><b>Comuniquese</b></h5>
+                                    {/* <ul className='redes'>
+                        <li><a href="https://www.facebook.com/SISEA"><i className="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://www.instagram.com/sisea_seguridad/"><i className="fab fa-instagram"></i></a></li>
+                        <li><a href="https://api.whatsapp.com/send?phone=50672920011"><i className="fab fa-whatsapp"></i></a></li>
+                        
+                    </ul> */}
+                                    {/* <a href = "https://www.facebook.com/SISEA/"> <i className = "fab fa-facebook"></i></a>
+                    <a href = "https://www.instagram.com/sisea_seguridad/"> <i className = "fab fa-instagram"></i></a>
+                    <a href = "https://api.whatsapp.com/send?phone=50672920011"> <i className = "fab fa-whatsapp"></i></a> */}
+
+                                    <div className="button" 
+                                        onClick={() => {
+                                            window.open("https://www.instagram.com/sisea_seguridad", "_blank");
+                                        }}>
+                                        <div className="icon"><i className="fab fa-instagram"></i></div>
+                                        <span>Instagram</span>
+                                    </div>
+                                    <div className="button"
+                                        onClick={() => {
+                                            window.open("https://www.facebook.com/SISEA", "_blank");
+                                        }}
+                                    >
+                                        <div className="icon"><i className="fab fa-facebook"></i></div>
+                                        <span>facebook</span>
+                                    </div>
+                                    <div className="button"
+                                        onClick={() => {
+                                            window.open("https://api.whatsapp.com/send?phone=50672920011", "_blank");
+                                        }}
+                                    >
+                                        <div className="icon"><i className="fab fa-whatsapp"></i></div>
+                                        <span>whatsapp</span>
                                     </div>
                                 </div>
-                            </section>
-                        </div>
+                                </div>
+
+                        </section>
+                    </div>
                     </div>
                 </div>
             </div>
