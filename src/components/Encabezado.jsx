36e9a3fd80cb980c6ecd77972305482bc705233b
novasuperior <lawrentvoli@gmail.com>
2022-09-22 20:30:12 -0600

    Arreglar la navbar y animacion de mobile

--- src/components/Encabezado.jsx
+++ src/components/Encabezado.jsx
@@ -1,46 +1,73 @@
-import React, { useState } from 'react'
-import { HashLink as Link } from 'react-router-hash-link'
-import '/src/css/Encabezado.css'
+import React, { useState } from "react";
+import { HashLink as Link } from "react-router-hash-link";
+import "/src/css/Encabezado.css";
 
 function Encabezado() {
   const [isMobile, setIsMobile] = useState(false);
   return (
     <>
-          <nav className='navbar'>
+      <nav className="navbar">
+        <div className="hard">
+          <img
+            src="src/img/Logo.png"
+            // width="100rem"
+            // height="100rem"
+            className="navbar-imagen"
+          />
+          <h4 className="logo">
+            <b>SISEA Seguridad</b>
+          </h4>
+        </div>
 
-              <img src='src/img/Logo.png'  width="100rem" height="100rem" 
-               className='navbar-imagen'
-              />
 
-            <h4 id='titulo' className='logo'><b>SISEA Seguridad</b></h4>
-            <ul className={isMobile ? "nav-links-mobile":"nav-links"}
-            onClick={()=>setIsMobile(false)}>
-                <Link to='/#' className='INICIO'>
-                    <li>Inicio</li> 
-                </Link>
-                <Link to='/#Servicios' className='SERVICIOS'>
-                    <li>Servicios</li>
-                </Link>
-                <Link to='/catalogo#Catalogo' className='CATALOGO'>
-                    <li>Catálogo</li>
-                </Link> 
-                <Link to='/#Informacion' className='INFORMACION'>
-                    <li>Información</li>
-                </Link>
-                <Link to='/#Publicaciones' className='PUBLICACIONES'>
-                    <li>Publicaciones</li>
-                </Link>
-                <Link to='/#Contacto' className='CONTACTO'>
-                    <li>Contacto</li>
-                </Link>
-            </ul>
-            <button className='mobile-menu-icon'
-            onClick={()=>setIsMobile(!isMobile)}>
-              {isMobile ? (<i className="fas fa-times"></i>) : ( <i className="fas fa-bars"></i>)}
-            </button>
-          </nav>
+        <ul
+          className={isMobile ? "nav-links-mobile" : "nav-links"}
+          onClick={() => setIsMobile(false)}
+        >
+          <li>
+            <Link smooth to="/#inicio" className="INICIO">
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="/#servicios" className="SERVICIOS">
+              Servicios
+            </Link>
+          </li>
+          <li>
+            <Link smooth to="/#contacto" className="CONTACTO">
+              Contacto
+            </Link>
+          </li>
+          <li>
+            <Link to="/#Informacion" className="INFORMACION">
+              Información
+            </Link>
+          </li>
+          <li>
+            <Link to="/#Publicaciones" className="PUBLICACIONES">
+              Publicaciones
+            </Link>
+          </li>
+          <li>
+            <Link to="/#Contacto" className="CONTACTO">
+              Contacto
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="mobile-menu-icon"
+          onClick={() => setIsMobile(!isMobile)}
+        >
+          {isMobile ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-bars"></i>
+          )}
+        </button>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Encabezado
+export default Encabezado;
