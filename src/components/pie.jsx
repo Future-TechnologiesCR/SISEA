62e0de42cd68c7bfed6c50c3bffb8bcbd7177c89
lawrent-yamil <lawrentvoli@gmail.com>
2024-09-11 21:11:19 -0600

    Cambios test 1

--- src/components/pie.jsx
+++ src/components/pie.jsx
@@ -1,15 +1,23 @@
 import '../css/pie.css'
 
 
-  const year = new Date().getFullYear()
+const year = new Date().getFullYear()
 
- function Pie() {
+function Pie() {
 
   return (
     <>
-        <div className='pie-body'>
-            <p>© {year} Creado por <b><a href='https://futuretechcr.com'>Future Technologies CR</a></b></p>
-        </div>
+      <div className='pie-body'
+        style={{
+          color: 'white',
+        }}
+      >
+        {/* <p>© {year} Creado por <b><a href='https://futuretechcr.com'>Future Technologies CR</a></b></p> */}
+
+        SISEA
+        Curridabat, San José, Costa Rica
+
+      </div>
     </>
   )
 }
